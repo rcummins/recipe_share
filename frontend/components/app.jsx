e330538d4c3f1474677fda6a1fb14c7aee7abac5
Renata Cummins <renatacummins@gmail.com>
2019-09-25 18:18:32 -0400

    feature: add login component as a frontend route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import SignupContainer from './session/signup_container';
+import LoginContainer from './session/login_container';
 
 const App = () => (
   <div>
     <Route path="/signup" component={SignupContainer} />
+    <Route path="/login" component={LoginContainer} />
   </div>
 );
 
