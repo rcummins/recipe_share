222381e169a30388b8eb8154d4e4bbf284f87ebe
Renata Cummins <renatacummins@gmail.com>
2019-09-25 11:53:41 -0400

    feature: add routes, controllers and JSON template for users and session

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password_digest, :session_token)
+  end
+end
