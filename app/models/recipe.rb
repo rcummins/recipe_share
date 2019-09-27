a30c29c1e1c7b752d7c46d0afc5e9a3840d9de60
Renata Cummins <renatacummins@gmail.com>
2019-09-27 12:21:58 -0400

    feature: validate that servings is a postive integer

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-  validates :servings, presence: true
+  validates :servings,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 0 }
 
   belongs_to :author, foreign_key: :author_id, class_name: :User
 end
