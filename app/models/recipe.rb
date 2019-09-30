d138407e4b8ab26f34a84294baf3a13ea9ad1417
Renata Cummins <renatacummins@gmail.com>
2019-09-30 15:39:00 -0400

    fix: destroy associated ingredients and instructions when recipe is destroyed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,6 @@ class Recipe < ApplicationRecord
     numericality: { only_integer: true, greater_than: 0 }
 
   belongs_to :author, foreign_key: :author_id, class_name: :User
-  has_many :ingredients
-  has_many :instructions
+  has_many :ingredients, dependent: :destroy
+  has_many :instructions, dependent: :destroy
 end
