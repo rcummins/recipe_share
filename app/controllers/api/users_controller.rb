6195121f4c96f38aef065ffbb84e67c032c813ff
Renata Cummins <renatacummins@gmail.com>
2019-10-03 14:08:35 -0400

    refactor: delete users show route

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,10 +10,6 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :recipes, only: [:index, :create, :show, :update, :destroy]
     resources :ingredients, only: [:create, :update, :destroy]
