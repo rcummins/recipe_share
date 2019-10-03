16799693641d83ef1cc4a0d02915a4d97422eab3
Renata Cummins <renatacummins@gmail.com>
2019-10-03 09:55:25 -0400

    fix: precent /recipes/new from interpreting 'new' as :recipeId wildcard

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
 
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import NavBarContainer from './nav_bar/nav_bar_container';
-import RecipeDetailContainer from './recipes/recipe_detail_container';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
 import CreateRecipeFormContainer from './recipes/create_recipe_form_container';
+import RecipeDetailContainer from './recipes/recipe_detail_container';
 
 const App = () => (
   <div className="app">
     <Route path="/" component={NavBarContainer} />
-    <Route path="/recipes/:recipeId" component={RecipeDetailContainer} />
     <AuthRoute path="/signup" component={SignupContainer} />
     <AuthRoute path="/login" component={LoginContainer} />
-    <ProtectedRoute path="/recipes/new" component={CreateRecipeFormContainer} />
+    <Switch>
+      <ProtectedRoute path="/recipes/new" component={CreateRecipeFormContainer} />
+      <Route path="/recipes/:recipeId" component={RecipeDetailContainer} />
+    </Switch>
   </div>
 );
 
