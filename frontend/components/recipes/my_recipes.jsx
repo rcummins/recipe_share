ddbfb429a191f2fe0c18dc1c190c77e538f7f14c
Renata Cummins <renatacummins@gmail.com>
2019-10-04 12:52:37 -0400

    feature: display message instead of empty table if user has not created any recipes

--- frontend/components/recipes/my_recipes.jsx
+++ frontend/components/recipes/my_recipes.jsx
@@ -11,39 +11,49 @@ class MyRecipes extends React.Component {
   }
 
   render() {
+    const { recipes } = this.props;
+
+    const display = recipes.length == 0 ? (
+      <div className="recipe-list-empty">
+        <p>You have not created any recipes yet!</p>
+      </div>
+    ) : (
+      <table>
+
+        <thead>
+          <tr>
+            <th className="col-recipe">Recipe</th>
+            <th className="col-rating">Taste rating</th>
+            <th className="col-rating">Effort rating</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {recipes.map((recipe, index) => (
+            <tr key={index}>
+              <td className="col-recipe">
+                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+              </td>
+              <td className="col-rating">
+                <p>{recipe.average_taste_rating}</p>
+              </td>
+              <td className="col-rating">
+                <p>{recipe.average_effort_rating}</p>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    )
+
     return(
       <div className="recipe-list">
         <Link
           className="link-add-recipe"
           to="/recipes/new">Add a new recipe</Link>
 
-        <table>
-
-          <thead>
-            <tr>
-              <th className="col-recipe">Recipe</th>
-              <th className="col-rating">Taste rating</th>
-              <th className="col-rating">Effort rating</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {this.props.recipes.map((recipe, index) => (
-              <tr key={index}>
-                <td className="col-recipe">
-                  <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-                </td>
-                <td className="col-rating">
-                  <p>{recipe.average_taste_rating}</p>
-                </td>
-                <td className="col-rating">
-                  <p>{recipe.average_effort_rating}</p>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-
-        </table>
+        { display }
       </div>
     )
   }
