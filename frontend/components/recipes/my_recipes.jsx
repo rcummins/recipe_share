206e071930ccafe136e6e525245c8b55c10b575c
Renata Cummins <renatacummins@gmail.com>
2019-10-11 14:08:33 -0400

    fix: if user has not created any recipes, don't show sort-by form

--- frontend/components/recipes/my_recipes.jsx
+++ frontend/components/recipes/my_recipes.jsx
@@ -29,94 +29,94 @@ class MyRecipes extends React.Component {
 
     const display = sortedRecipesArray.length == 0 ? (
       <div className="recipe-list-empty">
-        <p>You have not created any recipes yet!</p>
+        <p>You have not created any recipes.</p>
       </div>
     ) : (
-      <table>
+      <div className="recipe-list">
 
-        <thead>
-          <tr>
-            <th className="col-recipe">Recipe</th>
-            <th className="col-rating">Taste rating</th>
-            <th className="col-rating">Effort rating</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
+        <form className="sort-by">
+          <label htmlFor="sort-by-select">Sort recipes by:</label>
+          <select
+            id="sort-by-select"
+            defaultValue={sortBy}
+            onChange={this.handleChange}>
+            <option value="TASTE_ASC">Taste rating - low to high</option>
+            <option value="TASTE_DESC">Taste rating - high to low</option>
+            <option value="EFFORT_ASC">Effort rating - low to high</option>
+            <option value="EFFORT_DESC">Effort rating - high to low</option>
+          </select>
+        </form>
 
-        <tbody>
-          {sortedRecipesArray.map((recipe, index) => (
-            <tr key={index}>
-              <td className="col-recipe">
-                <Link
-                  className="link-recipe-detail"
-                  to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-              </td>
-              <td className="col-rating">
-                {
-                  recipe.average_taste_rating == '0.0' ? (
-                    <p>Not rated</p>
-                  ) : (
-                    <div className="rating-number-icon">
-                      <p>{Number
-                        .parseFloat(recipe.average_taste_rating)
-                        .toFixed(1)}</p>
-                      <img src={window.yellowStarURL} />
-                    </div>
-                  )
-                }
-              </td>
-              <td className="col-rating">
-                {
-                  recipe.average_effort_rating == '0.0' ? (
-                    <p>Not rated</p>
-                  ) : (
-                    <div className="rating-number-icon">
-                      <p>{Number
-                        .parseFloat(recipe.average_effort_rating)
-                        .toFixed(1)}</p>
-                      <img src={window.yellowKnifeURL} />
-                    </div>
-                  )
-                }
-              </td>
-              <td>
-                <Link
-                  className="link-edit-recipe"
-                  to={`/recipes/${recipe.id}/edit`}>Edit</Link>
-              </td>
-              <td>
-                <button
-                  className="button-delete-recipe"
-                  onClick={this.handleDelete(recipe)}>Delete</button>
-              </td>
+        <table>
+
+          <thead>
+            <tr>
+              <th className="col-recipe">Recipe</th>
+              <th className="col-rating">Taste rating</th>
+              <th className="col-rating">Effort rating</th>
+              <th></th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
+          </thead>
 
-      </table>
+          <tbody>
+            {sortedRecipesArray.map((recipe, index) => (
+              <tr key={index}>
+                <td className="col-recipe">
+                  <Link
+                    className="link-recipe-detail"
+                    to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+                </td>
+                <td className="col-rating">
+                  {
+                    recipe.average_taste_rating == '0.0' ? (
+                      <p>Not rated</p>
+                    ) : (
+                      <div className="rating-number-icon">
+                        <p>{Number
+                          .parseFloat(recipe.average_taste_rating)
+                          .toFixed(1)}</p>
+                        <img src={window.yellowStarURL} />
+                      </div>
+                    )
+                  }
+                </td>
+                <td className="col-rating">
+                  {
+                    recipe.average_effort_rating == '0.0' ? (
+                      <p>Not rated</p>
+                    ) : (
+                      <div className="rating-number-icon">
+                        <p>{Number
+                          .parseFloat(recipe.average_effort_rating)
+                          .toFixed(1)}</p>
+                        <img src={window.yellowKnifeURL} />
+                      </div>
+                    )
+                  }
+                </td>
+                <td>
+                  <Link
+                    className="link-edit-recipe"
+                    to={`/recipes/${recipe.id}/edit`}>Edit</Link>
+                </td>
+                <td>
+                  <button
+                    className="button-delete-recipe"
+                    onClick={this.handleDelete(recipe)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+      </div>
     )
 
     return(
       <div className="recipe-list-and-sort-by">
 
-        <div className="recipe-list">
-
-          <form className="sort-by">
-            <label htmlFor="sort-by-select">Sort recipes by:</label>
-            <select
-              id="sort-by-select"
-              defaultValue={sortBy}
-              onChange={this.handleChange}>
-              <option value="TASTE_ASC">Taste rating - low to high</option>
-              <option value="TASTE_DESC">Taste rating - high to low</option>
-              <option value="EFFORT_ASC">Effort rating - low to high</option>
-              <option value="EFFORT_DESC">Effort rating - high to low</option>
-            </select>
-          </form>
-
-          { display }
-        </div>
+        { display }
 
         <Link
           className="link-add-recipe"
